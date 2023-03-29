035235e9c647fd08bae0a34312b18174e2618ab2
Electric131 <39623923+Electric131@users.noreply.github.com>
2023-03-29 18:05:18 -0500

    Added navbar

--- projects/genetics-playground/index.js
+++ projects/genetics-playground/index.js
@@ -3,3 +3,30 @@
 // - Thick / Thin Fur coat
 // - Long / Short teeth
 // - Floppy / Short ears
+
+class Rabbit {
+    constructor() {
+        this.genetics = {}
+        this.position = {x: 0, y: 0}
+        this.mate = null
+    }
+
+    set_trait(name, value) {
+        this.genetics
+    }
+}
+
+class Population {
+    constructor() {
+        this.generation = 0
+        this.rabbits = []
+    }
+
+    add_trait(name) {
+
+    }
+}
+
+
+let rabbit = new Rabbit()
+console.log(rabbit.genetics)
